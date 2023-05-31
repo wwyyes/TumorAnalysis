14e53d3299afa3128562c75d840605d46b22ceb9
Wenya Wang / 王文雅 <48430084+wwyyes@users.noreply.github.com>
2023-05-31 12:10:19 +0200

    Create bestPractice.sh

---
+++ basicSomaticMutationDetection/Mutect2/bestPractice.sh
@@ -0,0 +1,2 @@
+# follow the best practice from GATK, but adapt it to own setting
+# Link: https://gatk.broadinstitute.org/hc/en-us/articles/360035894731-Somatic-short-variant-discovery-SNVs-Indels-
